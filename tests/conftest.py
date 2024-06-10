901d792efa90a85443ce35751914a3d7c330cace
abdeljalil.OUZZINE <34714128+jwilili@users.noreply.github.com>
2024-06-10 15:55:18 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
-from app.db.database import database, create_tables
+from app.database.db import database, create_tables
 
 @pytest.fixture(scope="module")
 def client():
